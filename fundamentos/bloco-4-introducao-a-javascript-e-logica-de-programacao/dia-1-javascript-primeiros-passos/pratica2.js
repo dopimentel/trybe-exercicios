0342dba054e74a8465fcfa020658274aa512bf9a
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-06-23 20:48:00 -0300

    Adiciona exercicios praticos do dia 1 do bloco 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/pratica2.js
@@ -0,0 +1,13 @@
+// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+// Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2 * base + 2 * height;
+console.log(perimeter);
+
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/pratica5.js
@@ -0,0 +1,11 @@
+// Crie uma variável para armazenar o estado da pessoa candidata no processo seletivo, que pode ser: 'aprovada', 'lista' ou 'reprovada';
+// Crie uma estrutura condicional com o switch/case que irá imprimir as mensagens do exercício anterior se o estado da pessoa candidata for 'aprovada', 'lista' ou 'reprovada'. Como default, imprima a mensagem de 'não se aplica'.
+
+
+
+let weekDay = "quarta-feira";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira"|| weekDay === "sexta-feira") {
+console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+console.log("FINALMENTE, descanso merecido UwU");
+}
