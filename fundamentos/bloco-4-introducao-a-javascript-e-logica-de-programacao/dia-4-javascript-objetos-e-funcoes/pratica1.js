f0a280334a3776722f2f2e6a4df6409e9dbd50bc
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-06-24 15:41:23 -0300

    1 - Crie um objeto player contendo as variáveis listadas abaixo.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica1.js
@@ -1,4 +1,6 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// 1 - Crie um objeto player contendo as variáveis listadas abaixo.
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
