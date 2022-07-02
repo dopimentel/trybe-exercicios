8ebb88f84ad6e7338b061d8b978db232657d8ea7
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-02 10:42:28 -0300

    Exercicio 1 e .git ignore

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -22,3 +22,36 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+function createDaysOfTheMonth () {
+    let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let days = document.getElementById('days');
+    for (let decemberDay of decemberDaysList) {
+        if (decemberDay === 24 || decemberDay === 31) {
+            let elementLi = document.createElement("li");
+            elementLi.innerHTML = decemberDay;
+            elementLi.className = "day";
+            elementLi.classList.add("day-holiday");
+            days.appendChild(elementLi);
+        } else if (decemberDay === 4 || decemberDay === 11 || decemberDay === 18) {
+            let elementLi = document.createElement("li");
+            elementLi.innerHTML = decemberDay;
+            elementLi.className = "day";
+            elementLi.classList.add("day-friday");
+            days.appendChild(elementLi);
+        } else if (decemberDay === 25) {
+            let elementLi = document.createElement("li");
+            elementLi.innerHTML = decemberDay;
+            elementLi.className = "day";
+            elementLi.classList.add("day-holiday");
+            elementLi.classList.add("day-friday");
+            days.appendChild(elementLi);
+        } else {
+            let elementLi = document.createElement("li");
+            elementLi.innerHTML = decemberDay;
+            elementLi.className = "day";
+            days.appendChild(elementLi);
+            }
+
+    };
+};
+createDaysOfTheMonth();
