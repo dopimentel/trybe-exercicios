d2d86677f3c0d3721f9b109aa49fd74a435370bc
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-12 16:13:53 -0300

    exercicio parte 2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script1.js
@@ -0,0 +1,16 @@
+btnEnviar = document.getElementById('btn-enviar');
+btnEnviar.addEventListener('click', capturaDoClick);
+
+function capturaDoClick(event) {
+    event.preventDefault();
+}
+
+
+
+
+
+// Crie um botão para enviar as informações preenchidas.
+// Agora vamos utilizar um pouco de javascript, para isso crie o arquivo script.js e o referencie no HTML.
+// Dica: Coloque a tag <script> no final do seu body.
+// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). Nossa amiga Carol Silva nos contou um pouco sobre como fazer isso, lembra?
+// Crie um botão que limpe as informações contidas nos campos;
