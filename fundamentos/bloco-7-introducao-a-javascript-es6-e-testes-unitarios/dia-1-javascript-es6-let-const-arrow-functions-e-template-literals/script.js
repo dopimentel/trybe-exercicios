8bdb998ea123f29997fdee2fad1e3b8cc488b5e0
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-17 18:41:12 -0300

    Modifique as variáveis para que respeitem o escopo onde estão declaradas

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,13 +1,18 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
-    ifScope = ifScope + " ótimo, fui utilizada no escopo !";
-    console.log(ifScope);
-  } else {
-    let elseScope = "Não devo ser utilizada fora meu escopo (else)";
-    console.log(elseScope);
-  }
-}
+      function testingScope(escopo) {
+        if (escopo === true) {
+          let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+          console.log(ifScope);
+        } else {
+          let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+          console.log(elseScope);
+        }
+      }
 
-testingScope(true);
-testingScope(false)
+      testingScope(true);
+
+// 🚀 Altere as variáveis para respeitarem o escopo em que foram declaradas.
+// Modifique a estrutura da função para que ela seja uma arrow function;
+// Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+// Modifique as concatenações para template literals.
+
