b53a01607de38362e6698ab2e2fce59179fdbd08
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-24 18:29:45 -0300

    Parte II - exercicio 3 -completo

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script1.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('btn');
+let clickCount = 0;
+
+const bodyElement = document.querySelector('body')
+const pElement = bodyElement.appendChild(document.createElement('p'));
+pElement.innerText =`${clickCount} click`;
+
+btn.addEventListener("click", (contador) => {
+  clickCount += 1;
+  console.log(clickCount);
+  let clicks = () => (clickCount > 1 ? "clicks" : "click");
+  pElement.innerText = `${clickCount} ${clicks()}`;
+  return clickCount;
+});
+
+
+
+
+// Crie uma página com um contador de cliques
+// Sua página deve conter:
+// Um botão ao qual será associado um event listener;
+// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
