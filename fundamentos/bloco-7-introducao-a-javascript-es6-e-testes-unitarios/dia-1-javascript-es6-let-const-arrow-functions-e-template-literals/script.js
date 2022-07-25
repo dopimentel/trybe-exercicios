e7cd2417a336765b8caa33c1673cc7283fc2e4ea
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-24 23:08:31 -0300

    Parte II - exercicio 4 - Função 1: Escreva uma função que substitua a letra x em uma frase

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -61,7 +61,7 @@ let longestWord = (phrase) => {
     let longest = phrase.split(' ')[0].length;
     let lWord = phrase.split(' ')[0];
     for (const word of phrase.split(' ')) {
-        if (word.length >= longest) {
+        if (word.length >   longest) {
             largest = word.length;
             lWord = word
         }
@@ -78,3 +78,37 @@ let longestWord2 = (phrase) => {
 
 console.log(longestWord2('Antônio foi no banheiro e não sabemos o que aconteceu'));
 
+
+// Crie duas funções JavaScript com as seguintes especificações:
+// Não se esqueça de usar template literals
+// Função 1: Escreva uma função que substitua a letra 'x' em uma frase.
+// A função deverá receber um nome por parâmetro;
+// Declare dentro da função uma variável com o nome frase, do tipo const e com o valor igual a 'Tryber x aqui!';
+// A função deverá retornar uma nova frase onde o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+// Exemplo:
+// Parâmetro: 'Bebeto'
+// Retorno: 'Tryber Bebeto aqui!'
+// Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+// A função deverá receber o retorno da Função 1 por parâmetro;
+// Declare dentro da função uma variável com o nome skills, do tipo const;
+// A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+// Concatene o valor retornado da Função 1, a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+// Exemplo de retorno:
+// Tryber Bebeto aqui!
+// Minhas três principais habilidades são:
+// JavaScript
+// HTML
+// CSS
+
+
+
+const replace = (nome) => {
+    const frase = "Tryber x aqui!";
+    let novaFrase = '';
+    for (const letter of frase) {
+        letter === 'x' ? novaFrase = `${novaFrase}${nome}` : novaFrase = `${novaFrase}${letter}`;
+    }
+    return novaFrase;
+}
+
+console.log(replace ('Joao'))
