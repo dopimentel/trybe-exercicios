5ded21be4d0e597cc55a239143bb51adc4e19d4f
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-26 11:29:07 -0300

    Exercicios Parte III - 5

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -128,5 +128,33 @@ const valuesSList = (object) => {
 console.log(valuesSList(lesson1));
 
 
+// 5 - Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+/*
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
+
+
+
+const allLessons = {};
+allLessons.lesson1 = Object.assign({}, lesson1);
+allLessons.lesson2 = Object.assign({}, lesson2);
+allLessons.lesson3 = Object.assign({}, lesson3);
+console.log(allLessons)
 
 
