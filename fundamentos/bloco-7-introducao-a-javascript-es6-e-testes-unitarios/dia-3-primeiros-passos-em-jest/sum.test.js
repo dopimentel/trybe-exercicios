2c2ab0bf61a4757d5d20b46c67d4255d29cc4fc7
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-27 06:36:20 -0300

    Exercicio 1 - Teste se o retorno de sum(0, 0) é 0

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -4,4 +4,9 @@ describe("The function sum(a, b) returns a plus b", () => {
     it("sum(4, 5) is 9", () => {
         expect(sum(4, 5)).toBe(9);
     });
+
+    it("sum(0, 0) is 0", () => {
+      expect(sum(0, 0)).toBe(0);
+    });
+
 });
