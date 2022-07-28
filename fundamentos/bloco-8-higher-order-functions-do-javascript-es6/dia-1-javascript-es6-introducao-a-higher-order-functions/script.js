860577dd42ed42388628e79155a185ee8db12d10
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-28 18:55:46 -0300

    Exercicio 1 - feito

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,10 +1,24 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
-const newEmployees = () => {
+const hiredPerson = (fullName) => {
+    const name = fullName.split(' ')[0].toLowerCase();
+    const lastName = fullName.split(' ')[fullName.split(' ').length - 1].toLowerCase();
+    return {
+        fullName,
+        email: `${name}_${lastName}@trybe.com`
+    };
+}
+
+const newEmployees = (callback) => {
   const employees = {
-    id1: "", // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: "", // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: "", // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   };
   return employees;
 };
+
+
+console.table(newEmployees(hiredPerson));
+
+
