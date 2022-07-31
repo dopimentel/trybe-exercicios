98952a357a1035104f3c429a6a423cb3a4bae7e9
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-30 21:59:47 -0300

    Exercicio 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio4.js
@@ -62,3 +62,35 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+// 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// const expectedResult = [
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+// ];
+
+const oldBooksOrdered = () => {
+  currentYear = new Date().getFullYear();
+  return books.filter((book) => currentYear - book.releaseYear > 60).sort((book1, book2) => book1.releaseYear - book2.releaseYear)
+
+}
+console.log(oldBooksOrdered())
