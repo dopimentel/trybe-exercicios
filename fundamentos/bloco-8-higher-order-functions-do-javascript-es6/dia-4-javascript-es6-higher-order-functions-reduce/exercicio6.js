7f0a4365bdd5d4207a51339cdc78ecda1fbbf1bd
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-31 22:40:43 -0300

    Exercicio 6 - Done

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
@@ -1,4 +1,9 @@
 // 6.**- Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos, aqui além de reduce será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected.
+// const expected = [
+//   { name: "Pedro Henrique", average: 7.8 },
+//   { name: "Miguel", average: 9.2 },
+//   { name: "Maria Clara", average: 8.8 },
+// ];
 
 const students = ["Pedro Henrique", "Miguel", "Maria Clara"];
 const grades = [
@@ -7,12 +12,15 @@ const grades = [
   [10, 7, 10, 8, 9],
 ];
 
-function studentAverage() {
-  // escreva seu código aqui
-}
+const studentAverage = () => students.map((student, index) => {
+  return {
+    name: student,
+    average:
+      grades[index].reduce((acc, grade) => acc + grade) /
+      grades[index].length,
+  };
+  });
 
-const expected = [
-  { name: "Pedro Henrique", average: 7.8 },
-  { name: "Miguel", average: 9.2 },
-  { name: "Maria Clara", average: 8.8 },
-];
+console.log(studentAverage(students))
+
+
