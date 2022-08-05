d6ac09a08fdd3bc90cae275aa09f979526e5ad3a
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-05 02:12:13 -0300

    Teste solicitado implementado

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio7.test.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio7.test.js
@@ -1,2 +1,17 @@
-const { describe } = require("yargs");
 const uppercase = require("./exercicio7");
+
+describe('Exercicico 7', () => {
+
+    it('Testa se ao receber a string "maria", temos "MARIA" no retorno', (done) => {
+        uppercase('maria', (str) => {
+            try {
+                expect(str).toBe('MARIA');
+                done();
+            } catch (error) {
+                done(error);
+            }
+            
+        })
+        
+    })
+})
