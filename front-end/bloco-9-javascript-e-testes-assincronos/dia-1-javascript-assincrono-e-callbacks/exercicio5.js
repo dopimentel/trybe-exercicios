6623a6e4accbe45403e65d9a0a4d2acb4848010c
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-05 00:42:52 -0300

    Exercicio 5 - Done

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio5.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio5.js
@@ -22,5 +22,8 @@ const greet = (temperature) =>
 
 // definição da função sendMarsTemperature...
 
+const sendMarsTemperature = (callback) => {
+    setTimeout(() => callback(getMarsTemperature()), messageDelay())
+};
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
