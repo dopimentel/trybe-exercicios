0d9cc379d06913153eb1bebd61005a0b9ef21d3a
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-13 17:12:27 -0300

    Refatoração e tratamento de erro

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -8,9 +8,16 @@ const fetchJoke = () => {
   };
 
   const joke = fetch(API_URL, myObject)
-    .then(response => response.json().then(data => {
-        const paragraph = document.getElementById("jokeContainer").innerText = data.joke;
-    }));
+    .then((response) => response
+    .json())
+    .then((data) => {
+      const paragraph = document.getElementById("jokeContainer");
+      paragraph.innerText = data.joke;
+    })
+    .catch((e) => {
+      const paragraph = document.getElementById("jokeContainer");
+      paragraph.innerText = `${e.message}. No more jokes for today`;
+    });
 };
 
 window.onload = () => fetchJoke();
