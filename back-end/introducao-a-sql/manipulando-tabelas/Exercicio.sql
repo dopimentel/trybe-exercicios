7a1132726764ad73b30c3fa095c5d95af17af214
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-08 01:59:57 -0300

    Adicione as informações à tabela box_office - Procurando Nemo

--- back-end/introducao-a-sql/manipulando-tabelas/Exercicio.sql
+++ back-end/introducao-a-sql/manipulando-tabelas/Exercicio.sql
@@ -1,8 +1,12 @@
 DESCRIBE movies;
 
-INSERT INTO pixar.movies (title, director, year, length_minutes)
+INSERT INTO pixar.movies(title, director, year, length_minutes)
 VALUES ('Monstros SA', 'Pete Docter', 2001, 91),
 ('Procurando Nemo', 'John Lasseter', 2003, 107),
 ('Os Incríveis', 'Brad Bird', 2004, 116),
-('WALL-E', 'Pete Docter', 2008, 104)
-;
+('WALL-E', 'Pete Docter', 2008, 104);
+
+DESCRIBE box_office;
+INSERT INTO pixar.box_office (movie_id, rating, domestic_sales, internationa_sales)
+VALUES(9, 6.8, 450000000, 370000000);
+
