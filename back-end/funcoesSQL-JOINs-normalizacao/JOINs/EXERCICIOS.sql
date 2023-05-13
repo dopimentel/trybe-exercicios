51ce1aedf5acfded1a258bdf22498e5c2fde8a77
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-13 10:59:20 -0300

    Utilizando o INNER JOIN, faça uma busca que  retorne os filmes e sua avaliação (rating) em ordem decrescente.

--- back-end/funcoesSQL-JOINs-normalizacao/JOINs/EXERCICIOS.sql
+++ back-end/funcoesSQL-JOINs-normalizacao/JOINs/EXERCICIOS.sql
@@ -7,3 +7,9 @@ SELECT m.id, m.title, bo.domestic_sales + bo.international_sales AS total_sales 
 INNER JOiN pixar.box_office AS bo
 ON m.id = bo.movie_id
 WHERE bo.international_sales > bo.domestic_sales;
+
+SELECT m.id, m.title, bo.rating FROM pixar.movies as m
+INNER JOiN pixar.box_office AS bo
+ON m.id = bo.movie_id
+ORDER BY rating DESC;
+
