338de095f1d247c716e1fc2d012a51571ba3d62d
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-21 18:05:53 -0300

    feat:exercici E - Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz

--- back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
+++ back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises;
 
-const readAll = async () => {
-  const json = await fs.readFile('./simpsons.json', 'utf-8');
+const readAll = async (path, encoding) => {
+  const json = await fs.readFile(path, encoding);
   const data = await JSON.parse(json);
   return data
 };
 
 const findCharacterById = async (inputIdInNumberOrString) => {
-  const data = await readAll();
+  const data = await readAll('./simpsons.json', 'utf-8');
   const promise = new Promise((resolve, reject) => {
     const idWasFinded = data.find(({ id }) => id == inputIdInNumberOrString);
     if (idWasFinded) {
@@ -18,10 +18,10 @@ const findCharacterById = async (inputIdInNumberOrString) => {
     }
   });
   return promise;
-}
+};
 
 const removeCharacterById = async (...ids) => {
-  const data = await readAll();
+  const data = await readAll('./simpsons.json', 'utf-8');
 
   const filteredData = data.filter(({ id }) => !ids.includes(Number(id)));
 
@@ -34,18 +34,32 @@ const removeCharacterById = async (...ids) => {
   const filteredDataJson = JSON.stringify(filteredData);
   console.log(filteredDataJson);
   await fs.writeFile('./simpsons.json', filteredDataJson);
-}
+};
 
 const simpsonFamily = async (...ids) => {
-  const data = await readAll();
+  const data = await readAll('./simpsons.json', 'utf-8');
   const filteredData = data.filter(({ id }) => ids.includes(Number(id)));
   const filteredDataJson = JSON.stringify(filteredData);
   console.log(filteredDataJson)
   await fs.writeFile('./simpsonFamily.json', filteredDataJson);
+};
+
+
+const addFamilyMember = async (name) => {
+  const data = await readAll('./simpsonFamily.json', 'utf-8');
+  console.log(data)
+  const newFamily =  [...data, {
+    id: String(data.length + 1),
+    name
+  }]
+  const newFamilyJson = JSON.stringify(newFamily);
+  console.log(newFamilyJson);
+  await fs.writeFile('./simpsonFamily.json', newFamilyJson);
 }
 
+
 const main = async () => {
-  const data = await readAll();
+  const data = await readAll('./simpsons.json', 'utf-8');
   // console.log(data)
   data.map((elem) => {
     const { id, name } = elem
@@ -63,6 +77,8 @@ const main = async () => {
   // removeCharacterById(10, 6);
 
   simpsonFamily(1, 2, 3, 4);
+
+  addFamilyMember('Nelson Muntz');
 
 };
 
