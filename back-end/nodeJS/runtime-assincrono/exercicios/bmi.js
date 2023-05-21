4614fa58320b1f53b8b3a3b3f75f225ed079170c
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-21 12:54:58 -0300

    Refac

--- back-end/nodeJS/runtime-assincrono/exercicios/bmi.js
+++ back-end/nodeJS/runtime-assincrono/exercicios/bmi.js
@@ -1,11 +1,12 @@
 const readline = require('readline-sync');
 
-const weightInKg = readline.questionFloat('What’s your weight?');
-const heightInCm = readline.questionInt('What’s your height?');
+const handleBMI = (weightInKg, heightInCm) => weightInKg / (heightInCm / 100) ** 2;
 
-const bmi = (weightInKg, heightInCm) => {
-  const result = weightInKg / (heightInCm / 100) ** 2;
-  console.log(`Your BMI is ${result}`);
-};
-bmi(weightInKg, heightInCm);
-module.exports = bmi;
+const main = () => {
+  const weightInKg = readline.questionFloat('What’s your weight?');
+  const heightInCm = readline.questionInt('What’s your height?');
+  const bmi = handleBMI(weightInKg, heightInCm);
+  console.log(`Your BMI is ${bmi}`);
+}
+main();
+
