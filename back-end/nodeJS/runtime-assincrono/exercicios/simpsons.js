e0da0dd7c6c1e5f5c4941ed35bf2f2fc180099d4
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-21 14:16:16 -0300

    feat: refac

--- back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
+++ back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
@@ -1,8 +1,13 @@
 const fs = require('fs').promises;
 
-const main = async () => {
+const readAll = async () => {
   const json = await fs.readFile('./simpsons.json', 'utf-8');
   const data = await JSON.parse(json);
+  return data
+};
+
+const main = async () => {
+  const data = await readAll();
   // console.log(data)
   data.map((elem) => {
     const { id, name } = elem
