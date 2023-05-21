dcfd10ea941aed4af470bbe202bcd0da4d38214c
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-21 12:42:09 -0300

    Exercicio 3 - Edite o código para que os valores de weight e height sejam informados pela pessoa ao responder as perguntas: “What’s your weight?” e “What’s your height?”. Deve-se utilizar o pacote readline-sync

--- back-end/nodeJS/runtime-assincrono/exercicios/bmi.js
+++ back-end/nodeJS/runtime-assincrono/exercicios/bmi.js
@@ -1,3 +1,11 @@
-const bmi = (weightInKg, heightInCm) => weightInKg / ((heightInCm/100) ** 2);
-console.log(bmi(64, 173));
+const readline = require('readline-sync');
+
+const weightInKg = readline.questionFloat('What’s your weight?');
+const heightInCm = readline.questionInt('What’s your height?');
+
+const bmi = (weightInKg, heightInCm) => {
+  const result = weightInKg / (heightInCm / 100) ** 2;
+  console.log(`Your BMI is ${result}`);
+};
+bmi(weightInKg, heightInCm);
 module.exports = bmi;
