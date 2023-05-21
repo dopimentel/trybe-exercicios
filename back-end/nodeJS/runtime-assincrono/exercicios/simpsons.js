10bbefcb3de647dc597a08254aaa0fccb4672ea9
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-21 14:13:46 -0300

    feat: Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson

--- back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
+++ back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
@@ -1,1 +1,13 @@
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const main = async () => {
+  const json = await fs.readFile('./simpsons.json', 'utf-8');
+  const data = await JSON.parse(json);
+  // console.log(data)
+  data.map((elem) => {
+    const { id, name } = elem
+    console.log(`${id} - ${name}`)
+  })
+
+};
+main();
