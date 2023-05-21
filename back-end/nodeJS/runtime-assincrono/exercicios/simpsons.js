effe382cbfec9fcf5cb94f1ce333fa5b091e43e0
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-21 15:09:38 -0300

    feat: Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com o nome da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo “id não encontrado”.

--- back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
+++ back-end/nodeJS/runtime-assincrono/exercicios/simpsons.js
@@ -6,13 +6,36 @@ const readAll = async () => {
   return data
 };
 
+const findCharacterById = async (inputIdInNumberOrString) => {
+  const data = await readAll();
+  const promise = new Promise((resolve, reject) => {
+    const idWasFinded = data.find(({ id }) => id == inputIdInNumberOrString);
+    if (idWasFinded) {
+      const { name: character } = idWasFinded;
+      resolve(character)
+    } else {
+      reject(new Error("id não encontrado"));
+    }
+  });
+  return promise;
+}
+
 const main = async () => {
   const data = await readAll();
   // console.log(data)
   data.map((elem) => {
     const { id, name } = elem
     console.log(`${id} - ${name}`)
-  })
+  });
+
+  try {
+    const character = await findCharacterById(100);
+    console.log(character);
+
+  } catch (error) {
+    console.error(error.message);
+  };
 
 };
+
 main();
