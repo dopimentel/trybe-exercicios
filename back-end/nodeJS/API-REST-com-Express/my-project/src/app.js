3dd233574f4d74b17269739dc27e827b1e130c10
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-22 17:26:28 -0300

    feat: exercico - Crie um endpoint do tipo DELETE com a rota /movies/:id que possa deletar um filme do JSON. e refac

--- back-end/nodeJS/API-REST-com-Express/my-project/src/app.js
+++ back-end/nodeJS/API-REST-com-Express/my-project/src/app.js
@@ -1,39 +1,29 @@
-const { log } = require('console');
 const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const MOVIES_PATH = path.resolve(__dirname,'./movies.json');
+const MOVIES_PATH = path.resolve(__dirname, './movies.json');
 const ENCODING = 'utf-8';
-
 app.use(express.json());
 
-const readAll = async (path, encoding) => {
+const readAll = async (filePath, encoding) => {
   try {
-    const json = await fs.readFile(path, encoding);
+    const json = await fs.readFile(filePath, encoding);
     const data = await JSON.parse(json);
-    console.log(data)
     return data;
   } catch (error) {
     console.error(`Arquivo não pôde ser lido: ${error}`);
   }
 };
 
-const updateMovies = async (path, data) => {
+const updateMovies = async (filePath, data) => {
   try {
-    await fs.writeFile(path, JSON.stringify(data));
-
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
   } catch (error) {
     console.error(`Arquivo não pôde ser atualizado: ${error}`);
-
   }
 };
-
-// async function main() {
-//   await readAll(MOVIES_PATH, ENCODING);
-// }
-// main();
 
 app.get('/movies/:id', async (req, res) => {
   try {
@@ -65,7 +55,7 @@ app.post('/movies/', async (req, res) => {
     const newMovie = {
       id: newIndex,
       movie,
-      price
+      price,
     };
     movies.push(newMovie);
     await updateMovies(MOVIES_PATH, movies);
@@ -85,13 +75,28 @@ app.put('/movies/:id', async (req, res) => {
       movieToUpdate.movie = movie || movieToUpdate.movie;
       movieToUpdate.price = price || movieToUpdate.price;
       await updateMovies(MOVIES_PATH, movies);
-      return res.status(200).json(movieToUpdate)
+      return res.status(200).json(movieToUpdate);
     }
     return res.status(404).json({ message: 'filme não encontrado' });
-
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-})
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await readAll(MOVIES_PATH, ENCODING);
+    const index = movies.findIndex(({ id: idElem }) => idElem === Number(id));
+    if (index === -1) {
+      return res.status(404).json({ message: 'filme não encontrado' });
+    }
+    movies.splice(index, 1);
+    await updateMovies(MOVIES_PATH, movies);
+    return res.status(204).end();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 module.exports = app;
