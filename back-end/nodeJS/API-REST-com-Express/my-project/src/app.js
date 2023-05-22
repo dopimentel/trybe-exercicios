3612e5efae9fc2f1504c20127fc9a38ed0003178
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-22 13:51:48 -0300

    feat: Crie um endpoint do tipo GET com a rota /movies/:id, que possa listar um filme do JSON por id

--- back-end/nodeJS/API-REST-com-Express/my-project/src/app.js
+++ back-end/nodeJS/API-REST-com-Express/my-project/src/app.js
@@ -2,27 +2,33 @@ const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
 
+const app = express();
 const MOVIES_PATH = path.resolve(__dirname,'./movies.json');
 const ENCODING = 'utf-8';
+
+app.use(express.json());
 
 const readAll = async (path, encoding) => {
   try {
     const json = await fs.readFile(path, encoding);
     const data = await JSON.parse(json);
-    console.log(data);
+    console.log(data)
     return data;
   } catch (error) {
     console.error(`Arquivo não pôde ser lido: ${error}`);
   }
-
 };
 
-async function main() {
-  await readAll(MOVIES_PATH, ENCODING);
-}
+// async function main() {
+//   await readAll(MOVIES_PATH, ENCODING);
+// }
+// main();
 
-main();
-
-const app = express();
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const movies = await readAll(MOVIES_PATH, ENCODING);
+  const movie = movies.find(({ id: movieId }) => movieId === Number(id));
+  return res.status(200).json(movie);
+});
 
 module.exports = app;
