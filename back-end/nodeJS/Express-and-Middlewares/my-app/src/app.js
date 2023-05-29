167be06906787b7f5c2ba3e9cc4c8b54ae7ad6ea
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-28 22:57:56 -0300

    feat: create nameValidation middleware

--- back-end/nodeJS/Express-and-Middlewares/my-app/src/app.js
+++ back-end/nodeJS/Express-and-Middlewares/my-app/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const activitiesRouter = require('./routes/activitiesRouter');
+const nameValidation = require('./middlewares/nameValidation');
 
 const app = express();
 app.use(express.json());
 
+app.use(nameValidation);
 app.use('/activities', activitiesRouter);
-
 
 module.exports = app;
