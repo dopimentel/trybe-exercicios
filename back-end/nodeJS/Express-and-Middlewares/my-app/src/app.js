613a9dd692e8afb0fdcc8a71ecbfd9f4ac1c6b59
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-29 00:49:32 -0300

    feat: create priceValidation middleware

--- back-end/nodeJS/Express-and-Middlewares/my-app/src/app.js
+++ back-end/nodeJS/Express-and-Middlewares/my-app/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const activitiesRouter = require('./routes/activitiesRouter');
 const nameValidation = require('./middlewares/nameValidation');
+const priceValidation = require('./middlewares/priceValidation');
 
 const app = express();
 app.use(express.json());
 
 app.use(nameValidation);
+app.use(priceValidation);
 app.use('/activities', activitiesRouter);
 
 module.exports = app;
