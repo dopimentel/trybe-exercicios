370fd2cdf309b8c52025d65f6b15d329d62bdec1
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-04 17:44:37 -0300

    rotas feitas

--- back-end/ORM-e-Autenticacao/ORM/app-with-sequelize/src/app.js
+++ back-end/ORM-e-Autenticacao/ORM/app-with-sequelize/src/app.js
@@ -8,4 +8,14 @@ app.use(express.json());
 
 app.get('/user', User.getAll);
 
+app.get('/user/:id', User.getById);
+
+app.get('/user/search/:id', User.getByIdAndEmail);
+
+app.post('/user', User.createUser);
+
+app.put('/user/:id', User.updateUser);
+
+app.delete('/user/:id', User.deleteUser);
+
 module.exports = app;
