9ec730aa68664a1b7068a518157e79b13cefb82e
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-05 00:12:45 -0300

    middleware de erro

--- back-end/ORM-e-Autenticacao/ORM/app-api-livros/index.js
+++ back-end/ORM-e-Autenticacao/ORM/app-api-livros/index.js
@@ -12,4 +12,17 @@ app.post('/books', create);
 app.put('/books/:id', update);
 app.delete('/books/:id', exclude);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  const errorResponse = {
+    status: 'error',
+    statusCode,
+    message: err.message || 'Internal Server Error',
+  };
+  
+  res.status(statusCode).json(errorResponse);
+});
+
+
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
