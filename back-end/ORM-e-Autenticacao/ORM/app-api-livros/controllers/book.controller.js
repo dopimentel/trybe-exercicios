d9924f7db0fcef16a3448956a84a68dfbe59d599
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-04 23:37:42 -0300

    exercicio 7

--- back-end/ORM-e-Autenticacao/ORM/app-api-livros/controllers/book.controller.js
+++ back-end/ORM-e-Autenticacao/ORM/app-api-livros/controllers/book.controller.js
@@ -10,6 +10,20 @@ const getAll = async (req, res, next) => {
   }
 }
 
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const book = await BookService.getById(id);
+
+    if (!book) return res.status(404).json({ message: 'Not found' });
+
+    res.status(200).json(book);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAll,
 };
