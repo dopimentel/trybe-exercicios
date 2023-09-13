eab4a756be1aa60e9113be21a47228c003a46a7e
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-13 18:54:03 -0300

    refac

--- back-end/ORM-e-Autenticacao/autenticacao/hello-jwt/src/middlewares/validateBody.js
+++ back-end/ORM-e-Autenticacao/autenticacao/hello-jwt/src/middlewares/validateBody.js
@@ -1,9 +1,7 @@
 const { bodySchema } = require('./schemas');
 
 const validateBody = (body) => {
-  console.log(body);
   const { error } = bodySchema.validate(body);
-  console.log(error);
   return error;
 };
 
