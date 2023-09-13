06976a7ef39e30605daf8f5995bcbf2cac1b65d7
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-13 17:35:32 -0300

    feat: /POST /login - exercicio 2

--- back-end/ORM-e-Autenticacao/autenticacao/hello-jwt/src/controllers/login.js
+++ back-end/ORM-e-Autenticacao/autenticacao/hello-jwt/src/controllers/login.js
@@ -1,28 +1,25 @@
 const jwt = require('jsonwebtoken');
 const validateBody = require('../middlewares/validateBody');
-// const { User } = require('../models');
+
 const { JWT_SECRET } = process.env;
 
 const login = async (req, res, next) => {
   const { username, password } = req.body;
-  const error = validateBody({ username, password });
-  if (error) {
-    return next(error);
-  }
-  // const user = await User.findOne({ where: { email } });
-  // if (!user || !bcrypt.compareSync(password, user.password)) {
-    // return res.status(401).json({
-      // error: { message: 'Invalid fields' },
-    // });
-  // }
-  
   const jwtConfig = {
     expiresIn: '1h',
     algorithm: 'HS256',
   };
+  const error = validateBody({ username, password });
+  if (error) return next(error);
+  if (username === 'admin' && password !== 's3nh4S3gur4???') {
+    const err = new Error('Incorrect username or password');
+    err.statusCode = 401;
+    return next(err);
+  }
+  const admin = req.body.username === 'admin' && req.body.password === 's3nh4S3gur4???';
   const token = jwt.sign({ data: { 
     username,
-    admin: false,
+    admin,
    } }, JWT_SECRET, jwtConfig);
 
   return res.status(200).json({ token });
