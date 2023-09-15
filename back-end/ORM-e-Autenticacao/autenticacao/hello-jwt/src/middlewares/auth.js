8179d78afd448e3f2e455eba9dc405cb2f01feb8
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-14 23:14:47 -0300

    refac

--- back-end/ORM-e-Autenticacao/autenticacao/hello-jwt/src/middlewares/auth.js
+++ back-end/ORM-e-Autenticacao/autenticacao/hello-jwt/src/middlewares/auth.js
@@ -4,13 +4,13 @@ const extractToken = (authorization) => authorization.split(' ')[1];
 
 const auth = (req, _res, next) => {
   const { authorization } = req.headers;
-  const token = extractToken(authorization);
   if (!authorization) {
     const err = new Error('Token not found');
     err.statusCode = 401;
     return next(err);
   }
   try {
+    const token = extractToken(authorization);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.data;
   } catch (err) {
