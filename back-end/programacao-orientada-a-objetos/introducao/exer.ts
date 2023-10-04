7a3281a27e7ddc48caa6ebcfd6b1ec7ad67d4080
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-10-04 18:05:06 -0300

    feat: class Student

--- back-end/programacao-orientada-a-objetos/introducao/exer.ts
+++ back-end/programacao-orientada-a-objetos/introducao/exer.ts
@@ -1,1 +1,60 @@
-console.log("Exercício 1");
+
+class Student {
+  private _enrollment: string;
+  private _name: string;
+  private _examsGrades: number[];
+  private _assignmentsGrades: number[];
+
+  constructor(enrollment: string, name: string) {
+    this._enrollment = enrollment;
+    this._name = name;
+    this._examsGrades = [];
+    this._assignmentsGrades = [];
+  }
+
+  getEnrollment(): string {
+    return this._enrollment;
+  }
+
+  setEnrollment(value: string) {
+    this._enrollment = value;
+  }
+
+  getName(): string {
+    return this._name;
+  }
+
+  setName(value: string) {
+    if (value.length < 3) {
+      throw new Error('O nome deve conter no mínimo 3 caracteres.');
+    }
+
+    this._name = value;
+  }
+
+  getExamsGrades(): number[] {
+    return this._examsGrades;
+  }
+
+  setExamsGrades(value: number[]) {
+    if (value.length > 4) {
+      throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
+    }
+
+    this._examsGrades = value;
+  }
+
+  getAssignmentsGrades(): number[] {
+    return this._assignmentsGrades;
+  }
+
+  setAssignmentsGrades(value: number[]) {
+    if (value.length > 2) {
+      throw new Error(
+        'A pessoa estudante só pode possuir 2 notas de trabalhos.',
+      );
+    }
+
+    this._assignmentsGrades = value;
+  }
+}
