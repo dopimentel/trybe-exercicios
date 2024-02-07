fa095c279b31adf391d818ded77bcea1f2f342a1
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-07 19:01:46 -0300

    node introduction

---
+++ back-end/nodeJS/runtime-assincrono/my-node-project/readFileSync.js
@@ -0,0 +1,14 @@
+
+
+const fs = require('fs').promises;
+
+async function main() {
+  try {
+    const data = await fs.readFile('./arquivo.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+}
+
+main();
