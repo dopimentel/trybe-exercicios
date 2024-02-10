bb809c2648bf2967b7aee0a7a3c0ce0fbf11e94a
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-09 22:18:18 -0300

    binary search iterative

--- computer-science/algoritmos/busca-e-ordenacao/exercises.py
+++ computer-science/algoritmos/busca-e-ordenacao/exercises.py
@@ -90,3 +90,27 @@ for algorithm in [bubble_sort, merge_sort]:
         with Cronometro(f"{algorithm_name} com {input} entradas em ordem reversa"):
             algorithm(reversed_sorted[:input])
         print("*" * 50)
+
+
+def binary_search_iterative(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+if __name__ == "__main__":
+    with Cronometro("Busca binária iterativa"):
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        target = 10
+        print(binary_search_iterative(array, target))
+    print("*" * 50)
+
