2be012033f8a0bcf4913b870146ce49934eb1e59
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-09 22:01:34 -0300

    compare algorithms bubble sort and merge sort

--- computer-science/algoritmos/busca-e-ordenacao/exercises.py
+++ computer-science/algoritmos/busca-e-ordenacao/exercises.py
@@ -38,3 +38,55 @@ for algorithm in [selection_sort, insertion_sort]:
         with Cronometro(f"{algorithm_name} com {input} entradas em ordem reversa"):
             algorithm(reversed_sorted[:input])
         print("*" * 50)
+
+
+def bubble_sort(list):
+    for i in range(len(list)):
+        for j in range(len(list) - i - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+
+
+def merge_sort(list):
+    if len(list) > 1:
+        mid = len(list) // 2
+        left = list[:mid]
+        right = list[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                list[k] = left[i]
+                i += 1
+            else:
+                list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            list[k] = right[j]
+            j += 1
+            k += 1
+    return list
+
+
+for algorithm in [bubble_sort, merge_sort]:
+    algorithm_name = algorithm.__name__
+    for input in (10_000, 100_000, 1_000_000):
+        with Cronometro(f"{algorithm_name} com {input} entradas aleatórias"):
+            algorithm(randoms[:input])
+        with Cronometro(f"{algorithm_name} com {input} entradas ordenados"):
+            algorithm(sorted[:input])
+        with Cronometro(f"{algorithm_name} com {input} entradas em ordem reversa"):
+            algorithm(reversed_sorted[:input])
+        print("*" * 50)
