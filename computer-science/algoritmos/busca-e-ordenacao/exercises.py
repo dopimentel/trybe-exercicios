8948071878a36330027ec9a00e0c656374674dfd
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-09 22:23:53 -0300

    find the first false

--- computer-science/algoritmos/busca-e-ordenacao/exercises.py
+++ computer-science/algoritmos/busca-e-ordenacao/exercises.py
@@ -107,10 +107,32 @@ def binary_search_iterative(list, item):
     return -1
 
 
+# entrada: [True, True, True, True, False, False, False]  # saída: 4
+# entrada: [True, True, False, False, False, False, False]  # saída: 2
+
+
+def find_first_false(list):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess is False:
+            if list[mid - 1] is True:
+                return mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
 if __name__ == "__main__":
-    with Cronometro("Busca binária iterativa"):
+    with Cronometro("Binary search iterative"):
         array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         target = 10
         print(binary_search_iterative(array, target))
     print("*" * 50)
 
+    with Cronometro("Find first false"):
+        array = [True, True, True, True, False, False, False]
+        print(find_first_false(array))
