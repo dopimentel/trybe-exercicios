e3ef1de7d255274592b77f7ec9ff7d5554d37eec
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-24 19:27:17 -0300

    class HashMap

--- computer-science/estrutura-de-dados-II/hashmap-e-dict/HashMap.py
+++ computer-science/estrutura-de-dados-II/hashmap-e-dict/HashMap.py
@@ -7,8 +7,7 @@ class Employee:
 class HashMap:
 
     def __init__(self):
-        self._buckets = [None for i in range(10)]
-
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
@@ -16,16 +15,26 @@ class HashMap:
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
-
+        self._buckets[address].append(employee)
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address].name
     
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
+    
     def update_value(self, id_num, name):
         address = self.get_address(id_num)
-        self._buckets[address].name = name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                item.name = name
+                return
+        return None
 
 
     def has(self, id_num):
@@ -46,3 +55,48 @@ if __name__ == "__main__":
 
 
     print(hashmap.get_value(23))
+    print(hashmap.update_value(23, 'name5'))
+    print(hashmap.get_value(23))
+
+
+    # Instanciando a classe Dict
+employee_registry = {}
+
+# Inserindo dados
+# objeto[chave] = valor
+employee_registry[14] = 'name1'
+employee_registry[23] = 'name2'
+employee_registry[10] = 'name3'
+employee_registry[9] = 'name4'
+print(employee_registry)
+
+# Alterando o nome do id 10
+# objeto[chave] = novo_valor
+employee_registry[10] = 'name30'
+print(f"Novo valor do id 10, após a atualização: {employee_registry[10]}")
+
+
+# Instanciação do objeto vazio
+dict1 = {}
+dict2 = dict()
+
+# Instanciação com preenchimento inicial de dados
+dict3 = {1: 'name1', 2: 'name2'}
+print(f"Dicionário 1: {dict1}")
+print(f"Dicionário 2: {dict2}")
+print(f"Dicionário 3: {dict3}")
+
+# Inserção e Alteração
+# Se a chave não existir no dict, uma nova entrada será criada
+# Se já existir, o valor será sobreposto
+dict1[14] = 'name1'
+print(f"Novo dicionário 1, pós inserção/alteração: {dict1}")
+
+# Consulta e Remoção
+# Se a chave não existir no dict, causa erro
+name = dict1[14]
+del dict1[14]
+print(f"Dicionário 1 pós consulta e deleção: {dict1}")
+
+# print(dict1(30)) # KeyError
+
