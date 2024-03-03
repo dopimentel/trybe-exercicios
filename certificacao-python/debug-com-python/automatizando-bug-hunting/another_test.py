f4170bc24486ff68c42d9642c8d2ff7e46192dc4
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-03 03:28:10 -0300

    monkeypatch

--- certificacao-python/debug-com-python/automatizando-bug-hunting/another_test.py
+++ certificacao-python/debug-com-python/automatizando-bug-hunting/another_test.py
@@ -1,2 +1,16 @@
 def test_list_item_multiply(my_list):
     assert [item * 3 for item in my_list] == [6, 6, 9]
+
+
+def my_function():
+    return f"Você digitou {input('Digite algo: ')}!"
+
+
+def test_my_function(monkeypatch):
+    def mock_input(_):
+        return "pytest is awsome"
+
+    monkeypatch.setattr("builtins.input", mock_input)
+    output = my_function()
+
+    assert output == "Você digitou pytest is awsome!"
