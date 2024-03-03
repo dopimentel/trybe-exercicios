a8cd9fee7dd5bd47bbb4b4ef2b483242584ae437
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-03 03:14:21 -0300

    capsys

--- certificacao-python/debug-com-python/automatizando-bug-hunting/a_test.py
+++ certificacao-python/debug-com-python/automatizando-bug-hunting/a_test.py
@@ -4,3 +4,23 @@ def test_a_simple_test():
 
 def test_sum(my_list):
     assert sum(my_list) == 7
+
+
+def test_print_to_stdout(capsys):
+    print("Hello, world!")
+    captured = capsys.readouterr()
+    assert captured.out == "Hello, world!\n"
+    assert captured.err == ""
+    # print("Hello, world!", file=sys.stderr)
+    # captured = capsys.readouterr()
+    # assert captured.out == ""
+    # assert captured.err == "Hello, world!\n"
+
+
+def test_error_to_stderr(capsys):
+    import sys
+
+    sys.stderr.write("Error message\n")
+    captured = capsys.readouterr()
+    assert captured.err == "Error message\n"
+    assert captured.out == ""
