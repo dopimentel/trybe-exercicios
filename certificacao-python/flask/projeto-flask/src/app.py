89639d0eed88c1c651268a1aa8305d20fa889b25
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-04-04 14:52:15 -0300

    controller

--- certificacao-python/flask/projeto-flask/src/app.py
+++ certificacao-python/flask/projeto-flask/src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
+from controllers.jokes_controller import jokes_controller
 from os import environ
-
 from waitress import serve
 
 import requests
 
 app = Flask(__name__)
+app.register_blueprint(jokes_controller, url_prefix="/jokes")
 
 
 def get_random_joke():
@@ -21,9 +22,23 @@ def get_random_joke():
     return "No jokes available. Try again."
 
 
-@app.route("/api/joke")
-def joke():
+@app.route("/api/devjoke")
+def dev_joke():
     return jsonify({"joke": get_random_joke()})
+
+
+# joke_list = [
+#     "Did you know that 10+10 and 11+11 are the same? <br> "
+#     "Because 10+10 is twenty, and 11+11 is twenty too.",
+#     "Sabe como chama a sorveteria do Michel Teló? <br> Ice te Pego.",
+#     "Por que o espanador não luta caratê? <br> Porque ele luta capoeira",
+# ]
+
+
+# @app.route("/api/joke")
+# def joke():
+
+#     return jsonify({"joke": random.choice(joke_list)})
 
 
 def start_server(host: str = "0.0.0.0", port: int = 8000):
